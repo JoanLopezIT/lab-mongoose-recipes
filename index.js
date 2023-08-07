12efe0c956951d57e9bddcd87beec63fc1c220de
JoanLopezIT <joan.lopez.job@gmail.com>
2023-08-07 11:02:23 +0200

    done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// ESTA ES LA OPCION DE ASYNC AWAIT, MAS ABAJO COMENTADO LA OPCION .THIS
+
 const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
@@ -45,3 +47,69 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+  // ESTA OPCION ES CON .THIS EN VEZ DE ASYNC Y AWAIT, LA DEL PROFE
+
+
+//   const mongoose = require('mongoose');
+
+// // Import of the model Recipe from './models/Recipe.model.js'
+// const Recipe = require('./models/Recipe.model');
+// // Import of the data from './data.json'
+// const data = require('./data');
+
+// const MONGODB_URI = 'mongodb+srv://marcel:12345@cluster0.vsfi8s4.mongodb.net/recipe-app';
+
+// // Connection to the database "recipe-app"
+// mongoose
+//   .connect(MONGODB_URI)
+//   .then(x => {
+//     console.log(`Connected to the database: "${x.connection.name}"`);
+//     // Before adding any recipes to the database, let's remove all existing ones
+//     return Recipe.deleteMany()
+//   })
+//   .catch(error => {
+//     console.error('Error connecting to the database', error);
+//   })
+//   .then(() => {
+//     // Run your code here, after you have insured that the connection was made
+//     const newRecipe = {
+//       title: 'Paella',
+//       level: 'Amateur Chef',
+//       ingredients: ['Rice', "Rabbit", "Green Beans", " Broth", "Red chili"],
+//       cuisine: 'Paella',
+//       dishType: 'main_course',
+//       img: "https://marxfood.com/wp-content/uploads/mf-RabbitPaella-Feature-1-1200x800.jpg",
+//       duration: 30,
+//       creator: 'JuanDa'
+//     }
+//     return Recipe.create(newRecipe)
+//   })
+//   .then(data => console.log(data.title))
+//   .then(() => {
+//     return Recipe.insertMany(data)
+//   })
+//   .then((alltherecipies) => {
+//     alltherecipies.forEach((recipe) => {
+//       console.log(recipe.title)
+//     })
+//   })
+//   .then(() => {
+//     return Recipe.findOneAndUpdate({ title: 'Rigatoni alla Genovese' }, { duration: 100 }, { new: true })
+//   })
+//   .then(updatedRecipie => console.log('THIS RECIPE HAS BEEN UPDATED', updatedRecipie))
+
+//   .then(()=> {
+//     return Recipe.deleteOne({title: "Carrot Cake"})
+//   }) 
+//   .then((deleted)=> console.log("Recipe has been deleted!",deleted))
+//   .catch(error => {
+//     console.error('There is an error during the process: ', error);
+//   })
+//   .finally(()=>{
+//     mongoose.connection.close(()=>{
+//       console.log("db is closed!");
+//     })
+//   })
